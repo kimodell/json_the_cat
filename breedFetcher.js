6348d1af270774a92f67cc0d36a040f76775366b
Kim O'Dell <kimbo.odell@gmail.com>
2024-02-23 21:41:17 -0500

    reformat fetchBreedDescription function to use callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,23 +4,25 @@ const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
 
-  request(breed, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
       //call the callback with error or null if there is no error
       callback(error, null);
     } else {
       //convert string into an object
       const data = JSON.parse(body);
-      //check if array is not empty, and if it is not, print the first element
+      //check if array is not empty
       if (data.length > 0) {
-        console.log(data[0].description);
+        //Call callback with null as the error and description/result as second argument
+        callback(null, data[0].description);
       } else {
-        console.log('Breed not found');
+        //Call the callback error message and null description 
+        callback('Breed not found', null);
       }
     }
   });
 };
 
-module.exports {
+module.exports = {
   fetchBreedDescription
 };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchBreedDescription } = require('./breedFinder');
+const { fetchBreedDescription } = require('./breedFetcher');
 
 const breedName = process.argv[2];
 
