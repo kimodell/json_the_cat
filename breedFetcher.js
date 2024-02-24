2d9b1082235f0278df94c9eead60a43917232aeb
Kim O'Dell <kimbo.odell@gmail.com>
2024-02-23 21:03:21 -0500

    add index.js and function taking breedName and callback as arguments

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,18 +2,21 @@ const request = require('request');
 //declare variable to take command line arg for specific breed name
 const breed = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv.slice(2)}`;
 
+const fetchBreedDescription = function(breedName, callback) {
 
-request(breed, (error, response, body) => {
-  if (error) {
-    console.log('Error:', error);  // Print the error if one occurred
-  } else {
-    //convert string into an object
-    const data = JSON.parse(body);
-    //check if array is not empty, and if it is not, print the first element
-    if (data.length > 0) {
-      console.log(data[0].description);
+  request(breed, (error, response, body) => {
+    if (error) {
+      //call the callback with error or null if there is no error
+      callback(error, null);
     } else {
-      console.log('Breed not found');
+      //convert string into an object
+      const data = JSON.parse(body);
+      //check if array is not empty, and if it is not, print the first element
+      if (data.length > 0) {
+        console.log(data[0].description);
+      } else {
+        console.log('Breed not found');
+      }
     }
-  }
-});
+  });
+};
